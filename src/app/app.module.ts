103dd882777239ae93a61aa25f3cd059870c1fa9
Heeneth12 <heeneth123@gmail.com>
2024-04-23 18:03:27 +0530

    created chat room component and add ui to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent,
+    ChatRoomComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  data:any = 
+    {
+      roomID: 2353,
+      roomName : "Gaming Hub",
+      topic : "discussing about online gaming",
+      startTime: "6:30",
+      endTime : "7:30"
+    }
+  
+
+}
