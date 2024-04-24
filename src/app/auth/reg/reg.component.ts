4b60620a6f586c3945c25517f9e7075041e1e233
Heeneth12 <heeneth123@gmail.com>
2024-04-24 12:13:56 +0530

    created auth module login and reg

---
+++ src/app/auth/reg/reg.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reg',
+  templateUrl: './reg.component.html',
+  styleUrl: './reg.component.css'
+})
+export class RegComponent {
+
+}
