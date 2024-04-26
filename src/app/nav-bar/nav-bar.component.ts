3263bd6ecd1d23ed1bceddc093af2f21b58393dc
Heeneth12 <heeneth123@gmail.com>
2024-04-26 11:00:07 +0530

    edited nav bar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
 
+  user:boolean  = false;
+  menu:boolean = false;
+showDropdown: any;
+
+  ngOnInit(): void {
+    if (localStorage.getItem('jwtToken')) {
+    this.user = true;
+    console.log(this.user)
+    }
+  }
+
+
+  showDropDown(){
+
+    this.menu = !this.menu
+    console.log(this.menu)
+  }
+
 }
