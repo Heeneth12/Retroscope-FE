7db8fdc0b428f22952cda5300083f1544d545134
Heeneth12 <heeneth123@gmail.com>
2024-05-24 12:52:12 +0530

    added exit button and addde logic for logout

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,48 +1,49 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../shared.service';
+import { Router } from '@angular/router';
+import { environment } from '../../environment/environment';
+
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css'],
-  standalone : true,
-  imports : [CommonModule]
+  standalone: true,
+  imports: [CommonModule]
 })
-export class NavBarComponent {
-
-  userEmail:string|null = localStorage.getItem('userEmail')
-  user:boolean  = false;
-  menu:boolean = false;
+export class NavBarComponent implements OnInit {
+  userEmail: string | null = localStorage.getItem('userEmail');
+  user: boolean = false;
+  menu: boolean = false;
   showDropdown: any;
 
-  constructor(private sharedService: SharedService) {}
+  constructor(private sharedService: SharedService, private router: Router) {}
 
   onMyRoomsClick() {
     this.sharedService.setMyRoomsView(true);
   }
+
   ngOnInit(): void {
     if (localStorage.getItem('jwtToken')) {
-    this.user = true;
-    // console.log(this.user)
+      this.user = true;
     }
-
-   
-
-
   }
 
-
-  showDropDown(){
-    this.menu = !this.menu
-    console.log(this.menu)
+  showDropDown() {
+    this.menu = !this.menu;
+    console.log(this.menu);
   }
 
   logout() {
-    localStorage.removeItem('jwtToken');
-    localStorage.removeItem('userEmail');
-    localStorage.removeItem('userName');
-    localStorage.removeItem('userId');
-  }
+    const idToken = sessionStorage.getItem('token'); // Retrieve ID token from local storage
+    const logoutUrl = `${environment.idpLogoutUrl}?id_token_hint=${idToken}&post_logout_redirect_uri=${environment.homeUrl}`;
 
+    // Clear local session
+    sessionStorage.clear();
+    localStorage.clear();
+  
+    // Redirect to IdP logout URL
+    window.location.href = logoutUrl;
+  }
 }
